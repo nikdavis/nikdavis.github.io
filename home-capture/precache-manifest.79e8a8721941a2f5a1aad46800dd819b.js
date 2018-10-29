b0ef537895192a817b2c77edb25d3277b12b95e1
Nikolas Davis <nikdavis@pdx.edu>
2018-10-28 20:07:05 -0700

    Updates

---
+++ home-capture/precache-manifest.79e8a8721941a2f5a1aad46800dd819b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a499ac35ec14df5bf2",
+    "url": "/home-capture/static/js/runtime~main.42a499ac.js"
+  },
+  {
+    "revision": "1e6658d8ad5c57960411",
+    "url": "/home-capture/static/js/main.1e6658d8.chunk.js"
+  },
+  {
+    "revision": "b3d98b044a0f98501af3",
+    "url": "/home-capture/static/js/1.b3d98b04.chunk.js"
+  },
+  {
+    "revision": "1e6658d8ad5c57960411",
+    "url": "/home-capture/static/css/main.f7a10463.chunk.css"
+  },
+  {
+    "revision": "f0c7c9585d652bd2f9b6cb98cd893e62",
+    "url": "/home-capture/index.html"
+  }
+];
